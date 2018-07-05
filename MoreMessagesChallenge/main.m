28309043b40a06cadd9bc0f7819f6dbc4e3dae35
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-05 12:12:15 -0700

    This prints my full birthdate including the day

--- MoreMessagesChallenge/main.m
+++ MoreMessagesChallenge/main.m
@@ -8,10 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
+int main (int argc, const char * argv[])
+{
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1977];
+        [comps setMonth:11];
+        [comps setDay:19];
+        [comps setHour:13];
+        [comps setMinute:10];
+        [comps setSecond:0];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        NSLog(@"My date of birth is %@", dateOfBirth);
+        
+        // calculation f
+        //double secondsSinceEarlierDate = [laterDate timeIntervalSinceDate:earlierDate];
     }
     return 0;
 }
+
