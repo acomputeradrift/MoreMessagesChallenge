18a48896ecc68105c066a07d50d403fea14a5d25
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-05 12:33:41 -0700

    Completed and working challenge

--- MoreMessagesChallenge/main.m
+++ MoreMessagesChallenge/main.m
@@ -12,6 +12,11 @@ int main (int argc, const char * argv[])
 {
     @autoreleasepool {
         
+        // create an instance of todays date in receiver laterDate
+        
+        NSDate *laterDate = [NSDate date];
+        
+        // creates an instance of (and initializes) NSDateComoponents and passes in the arguments to receiver comps
         NSDateComponents *comps = [[NSDateComponents alloc] init];
         [comps setYear:1977];
         [comps setMonth:11];
@@ -19,13 +24,15 @@ int main (int argc, const char * argv[])
         [comps setHour:13];
         [comps setMinute:10];
         [comps setSecond:0];
-        
+        // creates an instance of (and initializes) NSCalendar pointer g
         NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        // creates an instance of NSDate dateOfBirth and calls method dateFromComponents with argument comps to receiver g
         NSDate *dateOfBirth = [g dateFromComponents:comps];
-        NSLog(@"My date of birth is %@", dateOfBirth);
+   
         
-        // calculation f
-        //double secondsSinceEarlierDate = [laterDate timeIntervalSinceDate:earlierDate];
+        // calculation from dateOfBirth to laterDate in seconds using timeIntervalSinceDate method
+        double secondsSinceBirthDate = [laterDate timeIntervalSinceDate:dateOfBirth];
+             NSLog(@"I have been alive for %f seconds", secondsSinceBirthDate);
     }
     return 0;
 }
